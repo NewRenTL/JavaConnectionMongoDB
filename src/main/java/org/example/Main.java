d32d8e38a3a68ef2bb0f24db3d6e85f327618fff
Brianmax <george.maxi@ucsp.edu.pe>
2024-01-19 13:55:14 -0500

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.db.MongoDBConnector;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MongoDBConnector.connect();
+        List<String> collections = MongoDBConnector.database.listCollectionNames().into(new ArrayList<>());
     }
 }
--- src/main/java/org/example/db/MongoDBConnector.java
+++ src/main/java/org/example/db/MongoDBConnector.java
@@ -4,21 +4,43 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import lombok.Getter;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class MongoDBConnector {
 
-    private MongoClient mongoClient;
+    private static MongoClient mongoClient;
     @Getter
-    private MongoDatabase database;
+    public static MongoDatabase database;
 
-    public MongoDBConnector(String connectionString, String databaseName) {
-        this.mongoClient = MongoClients.create(connectionString);
+    public static void connect() {
+        Properties properties = loadProperties();
+        String connectionString = properties.getProperty("mongodb.url");
+        String databaseName = properties.getProperty("mongodb.database");
 
-        this.database = mongoClient.getDatabase(databaseName);
+        mongoClient = MongoClients.create(connectionString);
+        database = mongoClient.getDatabase(databaseName);
     }
-    public void close() {
+    private static Properties loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream input = MongoDBConnector.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                System.out.println("Lo siento, no se puede encontrar el archivo de configuración");
+                System.exit(1);
+            }
+
+            properties.load(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        return properties;
+    }
+    public static void close() {
         if (mongoClient != null) {
             mongoClient.close();
         }
     }
 }
+
